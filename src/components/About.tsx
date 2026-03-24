50dbf9d60eec803ffc28ec60a226064b63e6cd26
goldenmeows <dominique@solorodesign.com>
2026-03-24 11:32:37 -0700

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -199,7 +199,7 @@ const styles = `
     width: 100%;
     height: 460px;
     object-fit: cover;
-    object-position: center bottom;
+    object-position: center top;
     display: block;
     transition: opacity 0.45s ease;
   }
