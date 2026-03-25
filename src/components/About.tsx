a3a4de5ada1ccff6891eb799473b4283ec752ac6
goldenmeows <dominique@solorodesign.com>
2026-03-24 21:36:00 -0700

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -90,7 +90,7 @@ const styles = `
     background: rgba(200,160,103,0.2);
   }
   .ab-divider-label {
-    font-family: 'Jost', sans-serif;
+    font-family: 'Cormorant, serif';
     font-weight: 400;
     font-size: 10px;
     letter-spacing: 0.38em;
